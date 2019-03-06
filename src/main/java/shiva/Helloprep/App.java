bb69b761cc43b130c3460ce3394a02bf3e2d14c7
shivakumarb3 <shivakumar.thadakamalla@outlook.com>
2019-03-06 23:02:25 +0530

    first maven push prep1

---
+++ src/main/java/shiva/Helloprep/App.java
@@ -0,0 +1,13 @@
+package shiva.Helloprep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String args[])
+    {
+        System.out.println( "Hello prep! welcome to github" );
+    }
+}
